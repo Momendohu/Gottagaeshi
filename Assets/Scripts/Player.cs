83d65e0e826983bf37feaacc155a9226bb3b8aa1
Momendohu <pinog.jh@gmail.com>
2018-08-18 16:35:52 +0900

    歩行テスト

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//using UnityEngine.UI;
+
+public class Player : MonoBehaviour {
+    //=============================================================
+    private Animator animator;
+
+    //=============================================================
+    private void Init () {
+        CRef();
+    }
+
+    //=============================================================
+    private void CRef () {
+        animator = GetComponent<Animator>();
+    }
+
+    //=============================================================
+    private void Awake () {
+        Init();
+    }
+
+    private void Start () {
+
+    }
+
+    private void Update () {
+        if(Input.GetKey(KeyCode.A)) {
+            animator.SetInteger("Speed",1);
+        }
+    }
+}
