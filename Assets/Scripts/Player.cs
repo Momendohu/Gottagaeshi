70c6fa100b12d268cb41765907d1ce78e2e8b3fb
Momendohu <pinog.jh@gmail.com>
2018-08-19 16:27:18 +0900

    ボタンで動作

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,6 +7,12 @@ public class Player : MonoBehaviour {
     //=============================================================
     private Animator animator;
 
+    private bool left;
+    private bool right;
+    private bool up;
+
+    private float turnSpeed = 2f;
+
     //=============================================================
     private void Init () {
         CRef();
@@ -28,18 +34,74 @@ public class Player : MonoBehaviour {
 
     private void Update () {
         if(Input.GetKey(KeyCode.RightArrow)) {
-            transform.eulerAngles += new Vector3(0,2,0);
+            right = true;
         }
 
         if(Input.GetKey(KeyCode.LeftArrow)) {
-            transform.eulerAngles += new Vector3(0,-2,0);
+            left = true;
         }
 
         if(Input.GetKey(KeyCode.UpArrow)) {
-            transform.position += new Vector3(Mathf.Sin(transform.eulerAngles.y*Mathf.Deg2Rad),0,Mathf.Cos(transform.eulerAngles.y * Mathf.Deg2Rad))*0.5f;
-            animator.SetInteger("Speed",2);
-        } else {
-            animator.SetInteger("Speed",0);
+            up = true;
         }
+
+        Debug.Log(up+":"+right+":"+left);
+        if(up) {
+            ForwardAction();
+        }
+
+        if(right) {
+            RightAction();
+        }
+
+        if(left) {
+            LeftAction();
+        }
+    }
+
+    //=============================================================
+    //右ボタン
+    public void RightButton () {
+        right = true;
+    }
+
+    //=============================================================
+    //左ボタン
+    public void LeftButton () {
+        left = true;
+    }
+
+    //=============================================================
+    //上ボタン
+    public void UpButton () {
+        up = true;
+    }
+
+    //=============================================================
+    //右押したときの行動
+    private void RightAction () {
+        transform.eulerAngles += new Vector3(0,turnSpeed,0);
+    }
+
+    //=============================================================
+    //左押したときの行動
+    private void LeftAction () {
+        transform.eulerAngles += new Vector3(0,-turnSpeed,0);
+    }
+
+    //=============================================================
+    //上押したときの行動
+    private void ForwardAction () {
+        transform.position += new Vector3(Mathf.Sin(transform.eulerAngles.y * Mathf.Deg2Rad),0,Mathf.Cos(transform.eulerAngles.y * Mathf.Deg2Rad)) * 0.5f;
+        animator.SetInteger("Speed",2);
+    }
+
+    //=============================================================
+    //行動初期化
+    public void InitializeAction () {
+        up = false;
+        left = false;
+        right = false;
+        animator.SetInteger("Speed",0);
     }
 }
